7261c1466ce5e88816191bd51dfea08f44df887a
donfortune <donfortunet.df@gmail.com>
2024-06-14 10:34:05 +0100

    jenkins-shared lib

--- script.groovy
+++ script.groovy
@@ -6,7 +6,17 @@ def testApp() {
     echo 'building the application...'
 }
 
+// def deployApp() {
+//     echo 'deploying the application...'
+//     echo "deploying version ${params.VERSION}"
+// }
+
 def deployApp() {
-    echo 'deploying the application...'
-    echo "deploying version ${params.VERSION}"
+    echo "Building Jar File"
+    withCredentials([usernamePassword(credentialsId: 'docker-credentials', passwordVariable: 'PWD', usernameVariable: 'USERNAME')]) {
+        
+        sh 'docker build -t donfortune1/my-repo:bukky-90.1 .'
+        sh "docker login -u $USERNAME -p $PWD"
+        sh 'docker push donfortune1/my-repo:bukky-90.1'
+    }
 }
